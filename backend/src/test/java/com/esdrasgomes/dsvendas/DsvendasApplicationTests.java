88b13e18bc96a666b8baf7142b197f2836f9f441
Esdras Gomes <esdrasgoomes@gmail.com>
2021-09-08 23:36:00 -0300

    created project

---
+++ backend/src/test/java/com/esdrasgomes/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esdrasgomes.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
